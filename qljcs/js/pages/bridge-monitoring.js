70e16ebb6f298787f78711d781e88c176543862c
slybootslion <slybootslion@vip.qq.com>
2021-11-05 14:48:20 +0800

    桥梁监测 20211105

--- qljcs/js/pages/bridge-monitoring.js
+++ qljcs/js/pages/bridge-monitoring.js
@@ -5,9 +5,37 @@ layui.use(['LuCommonTemplate', 'echarts'], function () {
 
   class PageTemplate {
     bridgeTemplate (data) {
+      console.log(data)
+      const instrumentDict = {
+        a1: { top: 50, left: 103 },
+        a2: { top: 453, left: 103 },
+        a3: { top: 90, left: 924 },
+        a4: { top: 227, left: 924 },
+        a5: { top: 242, left: 1334 },
+      }
+      const iconDict = {
+        1: { name: '位移计', icon: 'inst-icon01' },
+        2: { name: '加速度', icon: 'inst-icon02' },
+        3: { name: '温度计', icon: 'inst-icon03' },
+        4: { name: '湿度计', icon: 'inst-icon04' },
+        5: { name: '裂缝计', icon: 'inst-icon05' },
+      }
+      let html = ''
+      for (let i = 0; i < data.instrumentList.length; i++) {
+        const item = data.instrumentList[i]
+        const { top, left } = instrumentDict[item.id]
+        const { icon, name } = iconDict[item.type]
+        html += `<div class="instrument-item" data-id="${item.id}" style="top: ${top}px; left:${left}px;">
+                    <div class="inst-item-icon" style="background-image:url('/qljcs/images/page/qljc/${icon}.png');"></div>
+                    <div class="inst-item-info">
+                      <p>${name}</p>
+                      <p><span>${item.name}:${item.value}</span></p>
+                    </div>
+                  </div>`
+      }
       const { height } = $lulib.domWidthHeight('.bridge-box')
-      const width = Number(data.width * height / data.height)
-      return `<div class="bridge-pic" style="width: ${width}px; height: ${height - 10}px; background-image:url('${data.pic}');"></div>`
+      const width = parseInt(`${data.width * height / data.height}`)
+      return `<div class="bridge-pic" style="width: ${width}px; height: ${height - 10}px; background-image:url('${data.pic}');">${html}</div>`
     }
   }
 
@@ -41,4 +69,11 @@ layui.use(['LuCommonTemplate', 'echarts'], function () {
     const html = pt.bridgeTemplate(data[id])
     $(".bridge-box").html(html)
   }
+
+  function handleInstrument () {
+    const { id } = $(this).data()
+    console.log(id)
+  }
+
+  $lulib.methodProxy.bindMethodProxy([{ dom: 'body', domStr: '.instrument-item', method: handleInstrument }])
 })
