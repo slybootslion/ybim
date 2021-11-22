9137afca7d831ec33246f6cf7129c567b5bcfd6b
slybootslion <slybootslion@vip.qq.com>
2021-11-22 15:27:21 +0800

    2021-11-22 15:27:21

--- zhui/lib/jordan-stone/lu-lib/LuAdmin.js
+++ zhui/lib/jordan-stone/lu-lib/LuAdmin.js
@@ -1,8 +1,50 @@
 layui.define([], exports => {
+  const $ = layui.$
+  const luUtils = layui.LuUtils
 
   class LuHeader {
+    headerTemplate (data) {
+      const { buttonList, projectList, weather, id } = data
+      let btnHtml = ''
+      for (let i = 0; i < buttonList.length; i++) {
+        const item = buttonList[i]
+        const isActive = i === 0 ? 'active' : ''
+        btnHtml += `<div class="btn-item ${isActive}">${item.text}</div>`
+      }
+      const projectName = projectList.find(p => p.id === id).name
+      const weatherIconDict = {
+        1: 'icon-tianqitubiao_dayu', // 大雨
+        2: 'icon-tianqitubiao_qing', // 晴
+        3: 'icon-tianqitubiao_daxue', // 大雪
+        4: 'icon-tianqitubiao_xiaoxue', // 小雪
+        5: 'icon-tianqitubiao_xiaoyu', // 小雨
+        6: 'icon-tianqitubiao_zhongyu', // 中雨
+        7: 'icon-tianqitubiao_duoyun', // 多云
+      }
+      const date = luUtils.getFormatTime('YYYY年MM月DD日 dddd')
+
+      return `<div class="left">
+                <div class="logo"></div>
+                <div class="btn-list">${btnHtml}</div>
+              </div>
+              <div class="right">
+                <div class="project-list">
+                  <span class="iconfont icon-chanyeyuanqu"></span>
+                  <span>${projectName} >></span>
+                </div>
+                <div class="weather-box">
+                  <span class="iconfont ${weatherIconDict[weather.icon]}"></span>
+                  <div class="weather-info">
+                    <div class="top">${date}</div>
+                    <div class="bottom">${weather.text} ${weather.temperature}</div>
+                  </div>
+                </div>
+              </div>`
+    }
+
     init (opts) {
-      console.log(opts)
+      const hh = this.headerTemplate(opts)
+      $("#luHeader").html(hh)
     }
   }
 
--- zhui/lib/jordan-stone/lu-lib/LuUtils.js
+++ zhui/lib/jordan-stone/lu-lib/LuUtils.js
@@ -2,6 +2,10 @@ layui.define(exports => {
   const $ = layui.$
 
   class LuUtils {
+    getFormatTime (formatStr = 'YYYY年MM月DD日 HH:mm:ss', date = new Date()) {
+      return dayjs(date).format(formatStr)
+    }
+
     ajax (url, opts = {}, type = 'get') {
       return new Promise(resolve => {
         $.ajax({
@@ -15,6 +19,20 @@ layui.define(exports => {
           }
         })
       })
+    }
+
+    getHashParams () {
+      const hash = parent.location.hash
+      if (!hash) return
+      const paramsStr = hash.split('?')[1]
+      if (!paramsStr) return {}
+      const paramArr = paramsStr.split('&')
+      const params = {}
+      paramArr.forEach(str => {
+        const arr = str.split('=')
+        params[arr[0]] = arr[1]
+      })
+      return params
     }
   }
 
