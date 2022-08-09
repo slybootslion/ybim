5ad71f164318b7560deb217d7c5e54a9f76a9fdf
slybootslion <slybootslion@vip.qq.com>
2022-08-09 14:10:39 +0800

    2022-08-09 14:10:39

--- bpjcs/js/pages/index.js
+++ bpjcs/js/pages/index.js
@@ -1,3 +1,34 @@
 layui.use([], function () {
   const $ = layui.$
+  const form = layui.form
+
+  checkboxRender()
+  const checkItems = $(".layui-form .check-item")
+  const checkAll = $(".layui-form .check-all")
+  form.on('checkbox', function (data) {
+    const itemName = data.elem.name
+    const checked = data.elem.checked
+    if (itemName === 'checkAll') {
+      checkItems.each((idx, el) => {
+        el.checked = checked
+      })
+    } else {
+      if (checked) {
+        let count = 0
+        checkItems.each((idx, el) => {
+          if (el.checked) count++
+        })
+        if (count === 5) {
+          checkAll[0].checked = checked
+        }
+      } else {
+        checkAll[0].checked = checked
+      }
+    }
+    checkboxRender()
+  })
+
+  function checkboxRender () {
+    form.render()
+  }
 })
