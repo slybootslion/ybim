564d875f9b89b0a461c43d4950c95b15b1243a92
slybootslion <slybootslion@vip.qq.com>
2022-10-26 14:45:30 +0800

    2022-10-26 14:45:30

--- htmls/js/pages/bim/machinery-manage/equipment-monitoring.js
+++ htmls/js/pages/bim/machinery-manage/equipment-monitoring.js
@@ -5,8 +5,9 @@ layui.use(['LuCommonTemplate', 'LuLayer'], function () {
 
   const LuInnerHeader = layui.LuInnerHeader
   const LuSearchForm = layui.LuSearchForm
+  const LuTable = layui.LuTable
 
-  let luInnerHeader
+  let luInnerHeader, luTable
 
   const equipmentList = [
     { id: 1, title: '路面机械' }, { id: 2, title: '起重机械' }, { id: 3, title: '混凝土机械' },
@@ -51,6 +52,62 @@ layui.use(['LuCommonTemplate', 'LuLayer'], function () {
     ])
   }
 
-  function tableRender () {
+  async function tableRender () {
+    const tableData = await $lulib.getMockData('/htmls/mock/bim/equipmentTableData.json', 17, '', false)
+    const template = `
+      <span>
+        <div class="table-statue">
+          <span class="point {{d.t7 === '正常' ? 'green' : 'red'}}"></span>
+          <span class="{{d.t7 === '正常' ? '' : 'red-text'}}" lay-event>
+            {{d.t7}}
+          </span>
+        </div>
+      </span>
+    `
+    const tableOptions = {
+      cols: [
+        $lulib.tableSetCenter([
+          { type: 'checkbox', width: 50 },
+          { field: 'id', title: '序号', width: 60 },
+          { field: 't1', title: '设备名称', minWidth: 180 },
+          {
+            title: '设备图片',
+            width: 120,
+            templet: '<span><div class="table-pic"><img src="{{d.t2}}" alt=""></div></span>',
+          },
+          { field: 't3', title: '设备编号', minWidth: 120 },
+          { field: 't11', title: '设备类型', width: 100 },
+          { title: '设备状态', width: 110, templet: template },
+          { field: 't12', title: '持续时长', width: 130 },
+          { field: 't13', title: '当前油量', minWidth: 130 },
+        ]),
+      ],
+      ctrlData: [
+        { eventStr: 'm1', txtStr: '工时' },
+        { eventStr: 'm2', txtStr: '轨迹' },
+        { eventStr: 'm3', txtStr: '油量' },
+        { eventStr: 'm4', txtStr: '定位' }
+      ],
+      methods: {
+        m1, m2, m3, m4
+      },
+    }
+    luTable = new LuTable(tableData, tableOptions)
+  }
+
+  function m1 (data) {
+
+  }
+
+  function m2 (data) {
+
+  }
+
+  function m3 (data) {
+
+  }
+
+  function m4 (data) {
+
   }
 })
