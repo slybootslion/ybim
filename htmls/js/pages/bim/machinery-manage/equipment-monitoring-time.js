bd525dff4d9d66cbba2d7ddf0c9ead219f03ce37
slybootslion <slybootslion@vip.qq.com>
2022-11-02 16:07:15 +0800

    2022-11-02 16:07:15

--- htmls/js/pages/bim/machinery-manage/equipment-monitoring-time.js
+++ htmls/js/pages/bim/machinery-manage/equipment-monitoring-time.js
@@ -44,132 +44,119 @@ layui.use(['LuCommonTemplate', 'laypage', 'echarts'], function () {
     console.log(data)
   }
 
-  // https://segmentfault.com/q/1010000041860085
   async function echartsRender () {
-    let yData = []; // Y轴项目类目
-    let dayTime = 3600 * 24 * 1000; // 一天的毫秒，因为01.01日-01.01日，也算一天
+    let yData = []
     let chartData = [
-      {
-        itemName: '2022-10-15', // 项目名
-        value: [
-          0, // 索引
-          1, // 项目开始时间
-          3, // 项目结束时间
-          1,
-        ]
-      },
-      {
-        itemName: '2022-10-15', // 项目名
-        value: [
-          0, // 索引
-          5, // 项目实际开始时间
-          7.6, // 项目实际结束时间
-          0,
-        ]
-      },
-      {
-        itemName: '2022-10-15', // 项目名
-        value: [
-          0, // 索引
-          7.9, // 项目实际开始时间
-          13.3, // 项目实际结束时间
-          1,
-        ]
-      },
-      {
-        itemName: '2022-10-17',
-        value: [
-          1,
-          14,
-          21.8,
-          1,
-        ]
-      },
-      {
-        itemName: '2022-10-18',
-        value: [
-          2,
-          7.54,
-          14.4,
-          1,
-        ]
-      },
-      {
-        itemName: '2022-10-18',
-        value: [
-          2,
-          4.1984,
-          5.2022,
-          0,
-        ]
-      },
-      {
-        itemName: '2022-10-19',
-        value: [
-          3,
-          9.9,
-          10.4,
-          1,
-        ]
-      },
-      {
-        itemName: '2022-10-20',
-        value: [
-          4,
-          0.4,
-          1,
-          1,
-        ]
-      },
-      {
-        itemName: '2022-10-21',
-        value: [
-          5,
-          1,
-          4,
-          1,
-        ]
-      },
-      {
-        itemName: '2022-10-22',
-        value: [
-          6,
-          2,
-          3,
-          1,
-        ]
-      },
-      {
-        itemName: '2022-10-22',
-        value: [
-          6,
-          6,
-          9,
-          0,
-        ]
-      },
-      {
-        itemName: '2022-10-23',
-        value: [
-          7,
-          5.5,
-          11,
-          1,
-        ]
-      },
-    ]
-
-    // const chartData = await $lulib.getMockData('/htmls/mock/bim/equipmentMonitoringTimeEchartsData.json', 1, '', false)
-
-    const formatYData = () => {
-      // const itemNameObj = {}
-      // $lulib.unique(chartData.map(i => i.itemName)).forEach((item, idx) => itemNameObj[item] = idx)
-      // console.log(itemNameObj)
-      // chartData.forEach(item => item.value.unshift(itemNameObj[item.itemName]))
-      yData = $lulib.unique(chartData.map(item => item.itemName))
-      console.log(yData)
-    }
-    formatYData()
+        {
+          itemName: '2022-10-15', // 项目名
+          value: [
+            0, // 索引
+            1, // 项目开始时间
+            3, // 项目结束时间
+            1,
+          ]
+        },
+        {
+          itemName: '2022-10-15', // 项目名
+          value: [
+            0,
+            5,
+            7.6,
+            0,
+          ]
+        },
+        {
+          itemName: '2022-10-15', // 项目名
+          value: [
+            0,
+            7.9,
+            13.3,
+            1,
+          ]
+        },
+        {
+          itemName: '2022-10-17',
+          value: [
+            1,
+            14,
+            21.8,
+            1,
+          ]
+        },
+        {
+          itemName: '2022-10-18',
+          value: [
+            2,
+            7.54,
+            14.4,
+            1,
+          ]
+        },
+        {
+          itemName: '2022-10-18',
+          value: [
+            2,
+            4.1984,
+            5.2022,
+            0,
+          ]
+        },
+        {
+          itemName: '2022-10-19',
+          value: [
+            3,
+            9.9,
+            10.4,
+            1,
+          ]
+        },
+        {
+          itemName: '2022-10-20',
+          value: [
+            4,
+            0.4,
+            1,
+            1,
+          ]
+        },
+        {
+          itemName: '2022-10-21',
+          value: [
+            5,
+            $lulib.randomInt(8, 3),
+            $lulib.randomInt(16, 11),
+            1,
+          ]
+        },
+        {
+          itemName: '2022-10-22',
+          value: [
+            6,
+            2,
+            3,
+            1,
+          ]
+        },
+        {
+          itemName: '2022-10-22',
+          value: [
+            6,
+            $lulib.randomInt(7, 4),
+            $lulib.randomInt(15, 9),
+            0,
+          ]
+        },
+        {
+          itemName: '2022-10-23',
+          value: [
+            7,
+            5.5,
+            11,
+            1,
+          ]
+        },
+      ]
+    ;(() => yData = $lulib.unique(chartData.map(item => item.itemName)))()
 
     const formatTooltip = params => {
       const values = params.map(p => p.value).reduce((per, cur) => {
@@ -180,53 +167,22 @@ layui.use(['LuCommonTemplate', 'laypage', 'echarts'], function () {
       const numToTime = num => {
         const timeArr = num.toString().split('.')
         let str = `${timeArr[0]}小时`
-        return timeArr.length < 1 ? str :
-          str + `${Math.floor(60 * (+timeArr[1] < 10 ? +timeArr[1] * 10 : +timeArr[1]) / 100)}分钟`
+        let minute = ''
+        timeArr[1] && (minute = Math.floor(60 * (+timeArr[1] < 10 ? +timeArr[1] * 10 : +timeArr[1]) / 100))
+        return timeArr.length < 2 ? str : +timeArr[0] === 0 ? `${minute}分钟` : str + `${minute}分钟`
       }
-
-      return `${params[0].name}:<br\>
-                  累计运行：${numToTime(values[1])}<br\>
+      return `${params[0].name}:<br/>
+                  累计运行：${numToTime(values[1])}<br/>
                   累计怠速运行：${numToTime(values[0])}`
     }
 
-    setData()
+    const opts = setOptions()
 
-    function setData () {
-      const param = {
-        title: {
-          text: '项目执行情况',
-          left: 'center'
-        },
+    function setOptions () {
+      return {
         tooltip: {
-          // 自定义提示信息
-          // params为当前点击图形元素的数据信息的对象
           formatter: formatTooltip,
-          // (params) {
-          // console.log(params[0])
-
-          // return 'jhks'
-          // // 计划开始时间
-          // let planStartDate = params[0].value[1]
-          // // 计划结束时间
-          // let planEndDate = params[0].value[2]
-          // // 实际开始时间
-          // let practiceStartDate = params[0].value[3]
-          // // 实际结束时间
-          // let practiceEndDate = params[0].value[4]
-          // // 项目周期(毫秒值)：计划结束日期 - 计划开始日期
-          // // eslint-disable-next-line
-          // let projectCycle_millisecond = +echarts.number.parseDate(params[0].value[2]) - +echarts.number.parseDate(params[0].value[1])
-          // // 项目周期(天数)
-          // let projectCycle_days = projectCycle_millisecond / dayTime + 1
-          // return params[0].name + '<br/>'
-          //   + '计划开始时间：' + planStartDate + '<br/>'
-          //   + '计划结束时间：' + planEndDate + '<br/>'
-          //   + '项目周期：' + projectCycle_days + '天<br/>'
-          //   + '实际开始时间：' + practiceStartDate + '<br/>'
-          //   + '实际结束时间：' + practiceEndDate
-          // }
         },
-        // 图表底板
         grid: {
           height: 460,
           tooltip: {
@@ -262,7 +218,6 @@ layui.use(['LuCommonTemplate', 'laypage', 'echarts'], function () {
         series: [
           {
             type: 'custom',
-            // 使用自定义的图形元素
             renderItem: renderItem,
             name: '运行',
             itemStyle: {
@@ -270,56 +225,35 @@ layui.use(['LuCommonTemplate', 'laypage', 'echarts'], function () {
               color: '#409EFF'
             },
             encode: {
-              // 将维度1和维度2的数据映射到x轴
               x: [1, 2],
-              // 将维度0的数据映射到y轴
               y: 0
             },
             data: chartData
           },
-          // 没有给它设置data，只是为了通过这个系列，显示图例(legend)而已
           {
             type: 'custom',
             name: '怠速',
             itemStyle: {
-              color: '#F56C6C'
+              color: '#ff9018'
             }
           }
         ]
       }
-      init(param)
     }
 
-// params为data中的数据项的信息对象 api是可调用的方法集合，可以对data中的数据项进行操作
     function renderItem (params, api) {
-      // 取出data中数据项的第一个维度的值
       let categoryIndex = api.value(0)
-      // ===============计划工期进度条
-      // 计划开始日期(在屏幕上的像素值)
-      // 将数据项中的数值对应的坐标系上的点，转换为屏幕上的像素值
-      // 坐标系上的点：是数据项映射到坐标系的x轴和y轴后，对应的位置
-      // 屏幕上的像素值：是坐标系上的点，在屏幕上的位置
       let planStartDate = api.coord([api.value(1), categoryIndex])
-      // 计划结束日期(在屏幕上的像素值)
       let planEndDate = api.coord([api.value(2), categoryIndex])
-      // 由于data.value中维度1和维度2的数据会被映射到x轴，而x轴的type为time，即时间轴，
-      // 所以api.value(1)和api.value(2)获取到的值是将日期转换后的毫秒值
-      // 设置图形的高度
-      // 获得Y轴上数值范围为1的一段所对应的像素长度；这是官方文档的注释，对于api.size()方法，目前我还不是很理解；先做个标记??? 以后再说
       let height = api.size([0, 1])[1] * 0.5
       let width = planEndDate[0] - planStartDate[0]
-      // 使用graphic图形元素组件，绘制矩形
-      // clipRectByRect方法，在绘制矩形时，如果矩形大小超出了当前坐标系的包围盒，则裁剪这个矩形
       let rectShape1 = echarts.graphic.clipRectByRect({
-          // 矩形的位置
           x: planStartDate[0],
           y: planStartDate[1] - height / 2,
-          // 矩形的宽高
           width,
           height
         },
         {
-          // 当前坐标系的包围盒
           x: params.coordSys.x,
           y: params.coordSys.y,
           width: params.coordSys.width,
@@ -329,31 +263,29 @@ layui.use(['LuCommonTemplate', 'laypage', 'echarts'], function () {
         type: 'group',
         children: [
           {
-            // 类型为矩形
             type: 'rect',
-            // 具体形状
             shape: rectShape1,
-            // 样式
             style: api.style({
-              fill: api.value(3) === 1 ? '#409EFF' : '#F56C6C'
+              fill: api.value(3) === 1 ? '#409EFF' : '#ff9018'
             })
           }
         ]
       }
     }
 
-    function init (param) {
-      echartsObj = echarts.init(document.querySelector('#echartsBox'), param)
-      echartsObj.setOption(param)
-    }
-
-    laypage.render({
-      elem: 'echartsPage',
-      limit: 8,
-      count: 51 //数据总数，从服务端得到
-    });
+    echartsObj = echarts.init(document.querySelector('#echartsBox'), opts)
+    echartsObj.setOption(opts)
   }
 
+  laypage.render({
+    elem: 'echartsPage',
+    limit: 8,
+    count: 51,
+    jump: (obj, first) => {
+      if (first) return
+      echartsRender()
+    }
+  });
 
   // tabs change
   const showExportTableButton = idx => {
