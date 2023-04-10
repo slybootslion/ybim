f42c2331161e4fe51d7a4e94b761bc745ffac219
slybootslion <slybootslion@vip.qq.com>
2023-04-10 16:36:31 +0800

    4月10日提交

---
+++ slope/src/views/system/components/sample-component.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { sampleItemI } from '@/views/system/role-method'
+import { setRolePowerData } from '@/views/system/role-method'
+
+const props = defineProps<{
+  tableData: sampleItemI[]
+  roleId: string
+}>()
+
+const changeRole = async (row: sampleItemI) => setRolePowerData({ role_id: props.roleId, data_id: row.data_id, data_level: row.data_level })
+</script>
+
+<template>
+  <el-table :data="tableData" border style="width: 100%">
+    <el-table-column property="data_name" label="全部" width="160" />
+    <el-table-column label="数据权限">
+      <template #default="scope">
+        <el-radio-group v-model="scope.row.data_level" class="ml-4" @change="changeRole(scope.row)">
+          <el-radio :label="0" size="large">
+            本人相关
+          </el-radio>
+          <el-radio :label="1" size="large">
+            本部门相关
+          </el-radio>
+          <el-radio :label="2" size="large">
+            本部门及以下部门相关
+          </el-radio>
+          <el-radio :label="3" size="large">
+            全部
+          </el-radio>
+        </el-radio-group>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
